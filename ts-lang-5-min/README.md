fd191850d65776b2aabd2161261100224fcbbf7b
baob <coder@onesandthrees.com>
2018-04-11 14:32:21 +0100

    base ts and compiled js

---
+++ ts-lang-5-min/README.md
@@ -0,0 +1,4 @@
+# README
+
+based on
+https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
---
+++ ts-lang-5-min/greeter.ts
@@ -0,0 +1,8 @@
+function greeter(person) {
+    return "hello " + person;
+}
+
+let user = "jane user"
+
+document.body.innerHTML = greeter(user);
+
