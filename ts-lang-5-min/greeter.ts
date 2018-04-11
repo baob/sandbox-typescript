2f15b8e9db579aaaece170e392b87db5675ffcf2
baob <coder@onesandthrees.com>
2018-04-11 15:51:58 +0100

    add classes

--- ts-lang-5-min/greeter.ts
+++ ts-lang-5-min/greeter.ts
@@ -1,3 +1,10 @@
+class Student {
+  fullName: string
+  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName
+  }
+}
+
 interface Person {
   firstName: string;
   lastName: string;
@@ -8,7 +15,6 @@ function greeter(person: Person) {
     return "hello " + person.firstName + " " + person.lastName ;
 }
 
-let user = { firstName: "jane", lastName: "user" }
+let user = new Student("Jane", "M.", "User");
 
 document.body.innerHTML = greeter(user);
-
